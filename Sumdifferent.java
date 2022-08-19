ed53c724791eb43ddf882b89d3e64403949bcf98
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-19 14:46:15 +0530

    Add files via upload

---
+++ Sumdifferent.java
@@ -0,0 +1,26 @@
+package operator1;
+//Q.3 Write a Java program that accepts two integers from the user and then
+//prints the sum, the difference, the product, the average, the distance (the
+//difference between integer), the maximum (the larger of the two integers), the
+//minimum (smaller of the two integers).
+
+public class Sumdifferent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("The two numbers are:");
+int a=25;
+int b=5;
+System.out.println(a+" "+b);
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println((a+b)/2);
+System.out.println(a-b);
+System.out.println(a>b&&a<b);
+
+	
+}
+	}
+
+
