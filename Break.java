98a4e033e1b6a322474bad474ac4836aa9125416
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-08-19 14:42:27 +0530

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,16 @@
+package operator1;
+//Q.4 Write a Java program to break an integer into a sequence of individual
+digits.
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The number is:");
+		int a=8432;
+		
+		System.out.println(8+" "+4+" "+3+" "+2);
+
+	}
+
+}
