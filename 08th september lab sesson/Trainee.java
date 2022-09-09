f5bb62c93a284821ad0e462fdf8876f191e9e16a
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-09-09 14:19:26 +0530

    Delete Trainee.java

--- 08th september lab sesson/Trainee.java
+++
@@ -1,9 +0,0 @@
-package lab08th;
-
-public class Trainee {
-	public static void main(String[] args) {
-			Employeee cc=new Employeee();
-			cc.Employeee(29846, "jack", "Mumbai India", 442085, 45000);
-			cc.calculateSalary();
-
-}}
