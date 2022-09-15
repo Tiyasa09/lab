d7c8f02af2c047f60d4cf1e8e99c4deb2dd5978c
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-09-15 21:10:48 +0530

    Add files via upload

---
+++ 15th sep/Bankrate.java
@@ -0,0 +1,36 @@
+package activity;
+class Bank{
+	float getInterest() {
+		return 0.0f;
+	}
+	}
+class SBI extends Bank{
+	float getInterest() {
+		return 8.0f;
+	
+	}
+}
+class ICICI extends Bank{
+	float getInterest() {
+		return 7.0f;
+	}
+}
+class AXIS extends Bank{
+	float getInterest() {
+		return 9.0f;
+	}
+}
+public class Bankrate {
+
+	public static void main(String[] args) {
+		SBI ss=new SBI();
+		System.out.println("SBI bank's rate is:"+ss.getInterest());
+		ICICI ii= new ICICI();
+		System.out.println("ICICI BANK'S RATE RATE IS:"+ii.getInterest());
+		AXIS aa=new AXIS();
+		System.out.println("AXIS bank's rate is:"+aa.getInterest());
+		
+
+	}
+
+}
