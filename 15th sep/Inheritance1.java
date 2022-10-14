395c664fb12ea01f10a2431208f2651029bf3c39
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-10-14 22:53:35 +0530

    Update Inheritance1.java

--- 15th sep/Inheritance1.java
+++ 15th sep/Inheritance1.java
@@ -4,16 +4,16 @@ package activity;
 //explain with an example)
 //this is an hierarchical inheritance
 class A{
-	void A() {
+	void A() { //base class
 		System.out.println("it is raining");
 	}
 }
-class B extends A{
+class B extends A{ //1st child class of parent class
 	void B() {
 		System.out.println("it is not raining");
 	}
 }
-class C extends A{
+class C extends A{ //2nd child class of parent class
 	void C() {
 		System.out.println("it may be raining");
 	}
@@ -22,12 +22,14 @@ class C extends A{
 public class Inheritance1 {
 
 	public static void main(String[] args) {
-		B bb=new B();
-		bb.B();
-		bb.A();
-		C cc=new C();
-		cc.C();
-		cc.A();
+		B bb=new B(); //creating object of class B
+		bb.B(); //calling void by object name
+		bb.A(); //calling void by object name
+		C cc=new C(); //creating object of class 
+		cc.C(); //calling void by object name
+		cc.A(); //calling void by object name
+		 
+		
 
 	}
 
