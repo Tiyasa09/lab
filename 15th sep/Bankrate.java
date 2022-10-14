244331797a80952a9b137ad30824b5d15ec4f3c0
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-10-14 22:49:47 +0530

    Update Bankrate.java

--- 15th sep/Bankrate.java
+++ 15th sep/Bankrate.java
@@ -1,21 +1,21 @@
 package activity;
-class Bank{
-	float getInterest() {
-		return 0.0f;
+class Bank{ //parent class
+	float getInterest() {  //method getinterest
+		return 0.0f;//return value
 	}
 	}
-class SBI extends Bank{
+class SBI extends Bank{ //1st child class
 	float getInterest() {
 		return 8.0f;
 	
 	}
 }
-class ICICI extends Bank{
+class ICICI extends Bank{ //2nd child class
 	float getInterest() {
 		return 7.0f;
 	}
 }
-class AXIS extends Bank{
+class AXIS extends Bank{ //3rd method
 	float getInterest() {
 		return 9.0f;
 	}
@@ -23,11 +23,11 @@ class AXIS extends Bank{
 public class Bankrate {
 
 	public static void main(String[] args) {
-		SBI ss=new SBI();
+		SBI ss=new SBI(); //creating object of sbi class
 		System.out.println("SBI bank's rate is:"+ss.getInterest()+"%");
-		ICICI ii= new ICICI();
+		ICICI ii= new ICICI(); //creating object of icici bank
 		System.out.println("ICICI bank's rate is:"+ii.getInterest()+"%");
-		AXIS aa=new AXIS();
+		AXIS aa=new AXIS(); //creating object of axis class
 		System.out.println("AXIS bank's rate is:"+aa.getInterest()+"%");
 		
 
