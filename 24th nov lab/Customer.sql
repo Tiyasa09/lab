f37096b1c56ae6ac5a9ff3a1aca0c702ed0cf88d
Tiyasa09 <110465702+Tiyasa09@users.noreply.github.com>
2022-11-27 21:22:24 +0530

    Update Customer.sql

--- 24th nov lab/Customer.sql
+++ 24th nov lab/Customer.sql
@@ -9,3 +9,19 @@ insert into customer values
 ('A05','Ravi','Shridhar','NA',null,'1989-02-15',500.50),
 ('A06','Rukmini','Aiyer','GH',5125274,'1987-07-23',1500.50);
 select * from customer;
+update customer set Phone=1234567 where Cust_id='A03';
+update customer set Phone=4567890 where Cust_id='A04';
+update customer set DOB='1990-08-12' where Cust_id='A03';
+update customer set DOB='1990-01-30' where Cust_id='A05';
+select * from customer;
+delete from customer where Cust_id='A01';
+delete from customer where Cust_id='A05';
+truncate table customer;
+insert into customer values
+('A01','Riya','Roy','MA',123456,'1990-01-15',860.50),
+('A02','Puja','Sanfui','CA',789012,'1991-12-20',700.75),
+('A03','Anu','Mondal','DV',345678,'1992-07-25',570.00),
+('A04','Bani','Saha','BB',null,'1993-02-28',890.00),
+('A05','Bisu','Ghosh','CC',543216,'1994-02-15',1200.50);
+update customer set Fname='tiyasa' where Lname='Roy';
+delete from customer where Lname='Roy';
